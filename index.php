eaf5907493f0f591ff65ba44199508ff778ce164
Yosuke C Sato <yosuke.c.sato@icloud.com>
2019-10-13 15:50:13 +0900

    クラスの作成

--- index.php
+++ index.php
@@ -5,4 +5,25 @@ $msg = "hello from the TOP!";
 echo $msg;
 var_dump($msg)
 
+class User {
+	
+	// property
+	public $name;
+
+	// constructor
+	public function __construct($name) {
+		$this->name = $name;
+	}
+	
+	// method
+	public function sayHi() {
+		echo "hi, I am $this->name!";
+	}
+}
+
+$tom = new User("Tom");
+$bob = new User("Bob");
+
+$bob->sayHi();
+
 ?>
