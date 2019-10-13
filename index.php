5eee31f8563d9fc7380197e6e263ec6065b93187
Yosuke C Sato <yosuke.c.sato@icloud.com>
2019-10-13 15:12:50 +0900

    できるかな

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php
 
-/*
-変数：データにつけるラベル
-
-データ型：
-- 文字列 string
-- 数値 integer float
-- 論理 boolean / true false
-- 配列
-- オブジェクト
-- null
-
-定数：変更されない値につけるラベル
-define("MY_EMAIL", "ythe@mail.com");
-
-
-*/
 
 $msg = "hello from the TOP!";
 echo $msg;
