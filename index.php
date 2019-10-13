2607b338ad202f39fc64c763b04e2527ed91f977
Yosuke C Sato <yosuke.c.sato@icloud.com>
2019-10-13 15:58:58 +0900

    test

--- index.php
+++ index.php
@@ -14,12 +14,22 @@ class User {
 	// method
 	public function sayHi() {
 		echo "hi, I am $this->name!";
+	}	
+}
+
+class AdminUser extends User {
+	public function sayHello() {
+		echo "hellow from Admin!";
 	}
 }
 
 $tom = new User("Tom");
 $bob = new User("Bob");
+$steve = new Admin("Steve");
 
 $bob->sayHi();
+$steve->sayHi();
+$steve->sayHello();
+
 
 ?>
